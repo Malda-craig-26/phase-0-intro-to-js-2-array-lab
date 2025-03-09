1be8b542b2fc2ed67a7d9cb7c0bd84797028ad65
kimberly <maldakimberly228@gmail.com>
2025-03-10 01:09:35 +0300

    create a file

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+cats.length = 0;
+cats.push('Milo', 'Otis', 'Garfield');
+function destructivelyAppendCat() {
+    cats.push('Ralph');
+}
+cats; 
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) {
+    return [...cats, name];
+
+}
+function prependCat(name) {
+   return [name, ...cats];
+}
+[ Array(4) ];
+function removeLastCat() {
+    return cats.slice(0, -1); 
+  }
+  function removeFirstCat() {
+    return cats.slice(1);
+  }
+  
+    
+
